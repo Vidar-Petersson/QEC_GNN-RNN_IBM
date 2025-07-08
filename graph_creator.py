c70b43d83bc0e6d7dc43d3d2129b8e08cea99cdd
Vidar Petersson <49240613+Vidar-Petersson@users.noreply.github.com>
2025-07-08 09:22:10 +0200

    Refactored graph_creator and added support for train/val splitting

--- graph_creator.py
+++ graph_creator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from tqdm import tqdm
-import time, itertools
+import time
 from args import Args
 from torch_geometric.nn.pool import knn_graph
 from dataloader_ibm import IBMSampler
@@ -9,12 +9,12 @@ from dataloader_ibm import IBMSampler
 class GraphCreator:
     """
     Class that is used to generate graphs of errors that occur in quantum computers. 
-    Call generate_batch() to generate a batch of graphs.
+    Call generate_batch() to generate batches of graphs.
     """
     def __init__(self, args: Args):
         self.device = args.device
         self.error_rates = args.error_rates 
-        self.batch_size = 1 # hardcoded args.batch_size
+        self.batch_size = args.batch_size
         self.t = args.t[0]
         self.dt = args.dt 
         self.distance = args.distance
@@ -23,24 +23,19 @@ class GraphCreator:
         self.k = args.k
         self.norm = args.norm
         self.simulator = args.simulator_backend
-        
-        if not self.sliding:
-            for t in args.t:
-                assert t % args.dt == args.dt - 1 # Syftet med detta?
-
-        # combinations = list(itertools.product(self.error_rates, self.t))
-        # if len(combinations) > 1:
-        #     print("Warning multiple errors and time steps inserted. Choosing only first to sample.")
-        #     self.t = self.t[0]
-        #     # Ignoring errors in the implementation with IBM backend
+        self.val_fraction = args.val_fraction
         
         t0 = time.perf_counter() 
         self.IBMSampler = IBMSampler(distance=self.distance, t=self.t, simulator=self.simulator)
         self.syndromes, self.flips = self.IBMSampler.load_jobdata() # Includes trivial syndromes, size as original file
         self.filename = self.IBMSampler.filename
+        
+        self.syndromes, self.flips = self.syndromes[:50000,:], self.flips[:50000,:] # Only for light testing
+        trivial_syndrome_mask = np.any(self.syndromes, axis=1) # Mask for trivial syndromes where no detection event happend
         t1 = time.perf_counter()
-        self.trivial_syndrome_mask = np.any(self.syndromes, axis=1) # Mask for trivial syndromes where no detection event happend
-        print(f"Loaded IBM jobdata (d={self.distance}, t={self.t}) with {self.flips.shape[0]} shots ({np.mean(~self.trivial_syndrome_mask)*100:.1f}% trivial) in {t1-t0:.2f} s.")
+        print(f"Loaded IBM jobdata {self.filename} (d={self.distance}, t={self.t}) with {self.syndromes.shape[0]} shots ({np.mean(~trivial_syndrome_mask)*100:.1f}% trivial) in {t1-t0:.2f} s.")
+        self.syndromes, self.flips = self.syndromes[trivial_syndrome_mask], self.flips[trivial_syndrome_mask]
+
 
         def _generate_detector_coordinates(d, t): # Lägga denna separat?
             d -= 1
@@ -50,7 +45,7 @@ class GraphCreator:
             return np.stack((col0, col1, col2), axis=1)
 
         self.detector_coordinates = _generate_detector_coordinates(self.distance, self.t)
-        self.stabilizer_mask = np.ones((1, self.distance-1), dtype=np.uint8) # Mask for type of stabiliser, not needed for repetition code
+        self.stabilizer_mask = np.ones((1, self.distance-1), dtype=np.uint8) # Mask for type of stabiliser, not exactly needed for the repetition code
         
     def get_sliding_window(self, node_features: list[np.ndarray], sampler_t: int
                         ) -> tuple[list[np.ndarray], np.ndarray]:
@@ -112,9 +107,6 @@ class GraphCreator:
             syndromes: Boolean array of shape [batch_size, s] where s is the total
                 number of detectors (t * number of stabilizers). Each entry indicates
                 whether a detection event occurred at a given space-time location.
-            sampler_idx: Index into the list of circuits/samplers. This determines 
-                which surface code configuration (e.g., error rate, number of rounds) 
-                was used for this batch.
 
         Returns:
             node_features: ndarray of shape [n, 5] where each row is (x, y, t, type_x, type_z).
@@ -122,29 +114,22 @@ class GraphCreator:
                 - type_x, type_z: one-hot encoding of stabilizer type
             batch_labels: ndarray of shape [n], mapping each node to a batch element
             chunk_labels: ndarray of shape [n], mapping each node to a time chunk (graph)
-        Note:
-            - When setting the number of QEC rounds to t, stim will return t + 1 X
-            stabilizers, and t - 1 Z stabilizers. In total, there are t + 1 'time 
-            steps'. This means, that there are g = t - dt + 2 chunks for each shot. 
         """
 
-        # Decode syndrome indices into (x, y, t) coordinates using precomputed Stim detector layout
+        # Decode syndrome indices into (x, y, t) coordinates using precomputed detector layout
         # Result: list of arrays, one per shot, each with shape [num_events_in_shot, 3]
-        node_features = [self.detector_coordinates[s] for s in syndromes] #Borde vara OK men dubbelkolla logiken
+        node_features = [self.detector_coordinates[s] for s in syndromes]
 
         if self.sliding:
             # Total number of rounds used in this circuit
-            sampler_t = self.t # self.circuits[sampler_idx].num_detectors // self.n_stabilizers
+            sampler_t = self.t
             # Apply a sliding window over time to divide events into overlapping chunks
             # Returns updated node_features with local time coordinates and chunk_labels
             node_features, chunk_labels = self.get_sliding_window(node_features, sampler_t)
         
         # Construct a batch_labels array that repeats batch indices according to number of events
         # Example: if shot 0 has 3 events and shot 1 has 5, this will be [0, 0, 0, 1, 1, 1, 1, 1]
-        # batch_labels = np.repeat(np.arange(len(node_features)), [len(i) for i in node_features])
-        num_nodes_per_shot = np.array([len(i) for i in node_features])
-        batch_indices = np.arange(len(node_features)).repeat(num_nodes_per_shot)
-        batch_labels = batch_indices
+        batch_labels = np.repeat(np.arange(len(node_features)), [len(i) for i in node_features])
 
         # Combine all node features into a single array [total_nodes, 3]
         node_features = np.vstack(node_features)
@@ -157,7 +142,6 @@ class GraphCreator:
             node_features[:, -1] = node_features[:, -1] % self.dt
 
         # Determine stabilizer type at each (x, y) coordinate using the precomputed mask
-        # stabilizer_mask == 3 indicates Z stabilizer; else it's X stabilizer
         stabilizer_type = self.stabilizer_mask[node_features[:, 1], node_features[:, 0]] == 3
         stabilizer_type = stabilizer_type[:, np.newaxis]  # Shape: [n, 1]
 
@@ -197,8 +181,7 @@ class GraphCreator:
         Returns:
             aligned_labels: Tensor of shape [B, L], aligned with GRU output
         """
-        # B = int(label_map[:, 0].max().item()) + 1 
-        B = max(int(label_map[:, 0].max().item()) + 1, 1)
+        B = int(label_map[:, 0].max().item()) + 1 
 
         lengths = torch.bincount(label_map[:, 0].long(), minlength=B)  # number of real chunks per batch
         max_len = lengths.max().item()
@@ -215,64 +198,68 @@ class GraphCreator:
 
         return aligned_flips, lengths  # counts = lengths for masking
 
+    def train_val_split(self, seed=None):
 
-    def generate_batch(self):
+        num_total = self.syndromes.shape[0]
+        val_size = int(num_total * self.val_fraction)
+
+        rng = np.random.default_rng(seed)
+        perm = rng.permutation(num_total)
+
+        # Indexera direkt med permuteringen
+        val_idx = perm[:val_size]
+        train_idx = perm[val_size:]
+
+        self.train_syndromes, self.train_flips = self.syndromes[train_idx], self.flips[train_idx] 
+        self.val_syndromes, self.val_flips = self.syndromes[val_idx], self.flips[val_idx]
+
+    def generate_batch(self, mode: str = "validation"):
         """
-        Generates a batch of graphs. 
+        Generates batches of graphs from the entire dataset, where each batch 
+        contains self.batch_size datapoints (i.e., shots).
 
-        Returns: 
-            node_features: tensor of shape [n, 5] ([x, y, t, (stabilizer type)]).
-            edge_index: tensor of shape [n_edges, 2]. Represents the edges, 
-                i.e. the adjacency matrix. 
-            labels: tensor of shape [n]. Represents which node features belong
-                to which combination of batch element and chunk. 
-                This is used when computing global_mean_pool following
-                graph convolutions. The reason being there is no 
-                explicit batch dimension. Therefore, a list of 
-                labels is needed to keep track of which node features
-                belong to which batch element. Further, each batch element
-                consists of multiple graphs, or chunks. Therefore, an integer
-                is assigned to each combination of batch element and chunk.
-            label_map: tensor of shape [n_graphs]. Maps labels to
-                [batch element, chunk].  
-            edge_attr: tensor of shape [n_edges]. Represents the edge weights.
-            flips: tensor of shape [batch_size, g]. Indicates if a logical 
-                bit- or phase-flip has occurred at the end of each chunk.
-            last_label: tensor of shape [batch_size, 1]. Indicates if a logical 
-                bit- or phase-flip has occurred at the end of the whole circuit.
-
-        Note:
-            - When setting the number of QEC rounds to t, stim will return t + 1 X
-              stabilizers, and t - 1 Z stabilizers. In total, there are t + 1 'time 
-              steps'. This means, that there are g = t - dt + 2 chunks for each shot. 
-            - Only logical observables measured at the end of each chunk are kept,
-              i.e., we discard the first `dt` entries and keep the final g entries.
-            - Because stim gives t logical observables, we copy the last label to
-              the last chunk ending at the last perfect stabilizer measurement
+        Returns:
+            List of batches. Each batch is a tuple:
+                node_features, edge_index, labels, label_map,
+                edge_attr, aligned_flips, lengths, last_label
         """
+
+        if mode == "validation":
+            syndromes = self.val_syndromes
+            flips = self.val_flips
+        elif mode == "training":
+            syndromes = self.train_syndromes
+            flips = self.train_flips
+
+        all_batches = []
+        perm = np.random.permutation(syndromes.shape[0])
+        syndromes = syndromes[perm]
+        flips = flips[perm]
         
-        batches = []
-        for i in range(0, self.syndromes.shape[0] - 1, 2):
-            syndromes_n = self.syndromes[i:i+2]
-            flips_n = self.flips[i:i+2]
+        # Keep only labels at chunk boundaries (i.e., end of each chunk)
+        flips = flips[:, self.dt - 1:]  # shape: [batch_size, g - 1], where g = t - dt + 2
+        flips = torch.from_numpy(flips).to(dtype=torch.float32, device=self.device)
+        # Append the last label one more time to get [B, g]
+        last_label = flips[:, -1:]  # shape [B, 1]
+        flips = torch.cat([flips, last_label], dim=1)  # shape [B, g]
 
-            print(syndromes_n)
-            print(flips_n)
-            
-            # Keep only labels at chunk boundaries (i.e., end of each chunk) 
-            flips_n = flips_n[:, self.dt - 1:]  # shape: [batch_size, g - 1], where g = t - dt + 2
-            flips_n = torch.from_numpy(flips_n).to(dtype=torch.float32, device=self.device)
-            last_label = flips_n[:, -1:]  # shape [B, 1]
-            flips_n = torch.cat([flips_n, last_label], dim=1)  # Append the last label one more time to get [B, g] # OK men varför?
+        num_total = syndromes.shape[0]
+        batch_size = self.batch_size
+
+        for i in tqdm(range(0, num_total, batch_size)):
+            synd_batch = syndromes[i:i+batch_size]
+            flips_batch = flips[i:i+batch_size]
+
+            # Hoppa över om batchen blir mindre än batch_size (t.ex. sista)
+            if synd_batch.shape[0] < batch_size:
+                break
 
             # Extract graph structure and labels for non-empty chunks
-            node_features, batch_labels, chunk_labels = self.get_node_features(syndromes_n)
+            node_features, batch_labels, chunk_labels = self.get_node_features(synd_batch)
             node_features = torch.from_numpy(node_features)
 
             # Map each unique (batch, chunk) pair to a unique graph index
-            # label_map = np.array(list(zip(batch_labels, chunk_labels)))
-            label_map = np.stack((batch_labels, chunk_labels), axis=1)
-
+            label_map = np.array(list(zip(batch_labels, chunk_labels)))
             label_map, counts = np.unique(label_map, axis=0, return_counts=True)
             labels = np.repeat(np.arange(counts.shape[0]), counts).astype(np.int64)
             label_map = torch.from_numpy(label_map)
@@ -282,7 +269,7 @@ class GraphCreator:
             edge_index, edge_attr = self.get_edges(node_features, labels)
 
             # align labels with chunk indices: 
-            aligned_flips, lengths = self.align_labels_to_outputs(label_map, flips_n)
+            aligned_flips, lengths = self.align_labels_to_outputs(label_map, flips_batch)
 
             # Move everything to the appropriate device
             node_features = node_features.to(self.device)
@@ -291,8 +278,9 @@ class GraphCreator:
             edge_index = edge_index.to(self.device)
             edge_attr = edge_attr.to(self.device)
             lengths = lengths.to(self.device)
+            last_label_batch = flips_batch[:, -1:]
 
-            batches.append((
+            all_batches.append((
                 node_features,
                 edge_index,
                 labels,
@@ -300,16 +288,17 @@ class GraphCreator:
                 edge_attr,
                 aligned_flips,
                 lengths,
-                last_label
+                last_label_batch
             ))
 
-        return batches
-    
+        return all_batches
+
 if __name__ == "__main__":
     args = Args(error_rates=[0.002], t=[21], distance=3, sliding=True, dt=2, simulator_backend=True)
     dataset = GraphCreator(args)
-    t0 = time.perf_counter()
     # node_features, edge_index, labels, label_map, edge_attr, aligned_flips, lengths, last_label = dataset.generate_batch()
+    t0 = time.perf_counter()
     batches = dataset.generate_batch()
-    print(batches)
+    print(f"Generated {len(batches)} batches of size {args.batch_size}")
+    print(f"First batch node_features shape: {batches[0][0].shape}")
     print(f"{time.perf_counter() - t0:.3f} seconds")
